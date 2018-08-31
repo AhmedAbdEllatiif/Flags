25d6cc5f6edcbe579e17a8c6fea8fbc4797bc74c
Ahmed <ahmedmohamedaneng@gmail.com>
2018-08-31 10:28:22 +0200

    improve_splash

--- app/src/main/java/com/example/ahmedd/flaglist/splash.java
+++ app/src/main/java/com/example/ahmedd/flaglist/splash.java
@@ -1,6 +1,7 @@
 package com.example.ahmedd.flaglist;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,26 +9,27 @@ import android.widget.Button;
 
 public class splash extends AppCompatActivity {
 
-    Button btnWelcome;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
-        btnWelcome = (Button) findViewById(R.id.btn_welcome);
-
-        btnWelcome.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
+            public void run() {
                 launchActivity();
             }
-        });
+        }, 2000);
+
+
     }
     private void launchActivity() {
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+        finish();
     }
 
 }
